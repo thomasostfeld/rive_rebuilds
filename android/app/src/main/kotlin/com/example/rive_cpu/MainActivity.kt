4b4ac749876e93d3e0224b95b4d00ac9efeeaead
Thomas Ostfeld <thomas.ostfeld@simpleclub.com>
2024-07-12 16:24:29 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/rive_cpu/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rive_cpu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 12,
+        itemBuilder: (context, index) {
+          return const SizedBox.square(
+            dimension: 100,
+            child: RiveAnimation.asset(
+              'assets/rive/square.riv',
+              animations: ['abc'],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
